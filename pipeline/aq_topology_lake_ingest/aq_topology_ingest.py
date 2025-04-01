77bdcdeeda4f6ff57c314bd6b2172a73bc8665e3
unknown <sam.haff.rn@gmail.com>
2025-04-01 03:11:38 +0300

    bring script back to life

--- pipeline/aq_topology_lake_ingest/aq_topology_ingest.py
+++ pipeline/aq_topology_lake_ingest/aq_topology_ingest.py
@@ -42,32 +42,15 @@ def openaq_locs():
         yield page
         print("Downloaded ~1000 locs.")
 
-current_sensor = -1
-@dlt.resource(name="openaq_measurements")
-def openaq_locs():
-    client = RESTClient(
-        base_url=f"https://api.openaq.org/v3/sensors/{current_sensor}/measurements",
-        headers={"X-API-Key":"932148dc9fced6a1df5c6d006c2ab3ae249eb6076ad539c693487236ace264dc"},
-        paginator=PageNumberPaginator(
-            base_page=1,
-            total_path=None
-        )
-    )
-
-    for page in client.paginate(params={"limit":"1000"}):
-        yield page
-        print("Downloaded ~1000 locs.")
-
-
 
 pipeline_name = "open_aq_locs_load"
 dataset_name = "sensors_topology"
 table_name = "locs"
 
 pipeline = dlt.pipeline(destination="filesystem", pipeline_name=pipeline_name, dataset_name=dataset_name)
-#load_info = pipeline.run(openaq_locs, table_name=table_name, loader_file_format="parquet", write_disposition="replace")
+load_info = pipeline.run(openaq_locs, table_name=table_name, loader_file_format="parquet", write_disposition="replace")
 
-#print("Download finished.")
+print("Download finished.")
 
 import pandas as pd
 
@@ -79,11 +62,11 @@ enr_df = sensors_df.set_index('_dlt_parent_id').join(locs_df.set_index('_dlt_id'
 selected_cols = ['id_sensor', 'name_sensor', 'parameter__id', 'parameter__name', 'parameter__units', 'parameter__display_name', 'id_loc', 'name_loc', 'timezone', 'country__id', 'country__code', 'country__name', 'coordinates__latitude', 'coordinates__longitude', 'datetime_first__utc', 'datetime_last__utc']
 sensors_final_df = enr_df[selected_cols]
 gs_topology_output_path = urljoin(gs_prod_data_path_url, "/sensors_topology/topology.parquet", ispath=False)
-#sensors_final_df.to_parquet(gs_topology_output_path)
+sensors_final_df.to_parquet(gs_topology_output_path)
 
-romania_country_id = 74
-romania_sensors_df = sensors_final_df[sensors_final_df.country__id == romania_country_id]
-print(romania_sensors_df)
-for t in romania_sensors_df.itertuples():
-    print(t)
-    break
+#romania_country_id = 74
+#romania_sensors_df = sensors_final_df[sensors_final_df.country__id == romania_country_id]
+#print(romania_sensors_df)
+#for t in romania_sensors_df.itertuples():
+#    print(t)
+#    break
