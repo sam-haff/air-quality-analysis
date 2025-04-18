32dcb59a99703b5eb6454a47eb8f73b63ef1166e
Semen Ahafontsev <sam.haff.rn@gmail.com>
2025-04-18 16:09:42 +0000

    correct safe levels for pollutants; fix latest measurements to only contain last 24h data.

--- dbt/macros/get_parameter_max_safe_value.sql
+++ dbt/macros/get_parameter_max_safe_value.sql
@@ -5,10 +5,10 @@ Returns maximum safe value for the selected pollutant
 {% macro get_parameter_max_safe_value(param) -%}
     case {{param}}
         when 'co' then 11500
-        when 'o3' then 50
+        when 'o3' then 140
         when 'pm10' then 54
         when 'no2' then 200
-        when 'pm25' then 12
+        when 'pm25' then 50
         when 'so2' then 197
     end
 {%-endmacro%}
