2685e5539a47fe9f9795c6e0775e0e167d063a03
Semen Ahafontsev <sam.haff.rn@gmail.com>
2025-04-18 20:56:35 +0000

    add analytics

--- dbt/models/staging/stg_measurements.sql
+++ dbt/models/staging/stg_measurements.sql
@@ -2,6 +2,8 @@
     config(materialized="view")
 }}
 
-select *
-from {{ source("staging", 'm_measurements') }}
+select m.*, l.country_name
+from {{ source("staging", 'm_measurements') }} as m
+join {{ ref("stg_locs")}} as l
+on l.id = m.location_id
 where value >= 0.0 and units = 'µg/m³'
